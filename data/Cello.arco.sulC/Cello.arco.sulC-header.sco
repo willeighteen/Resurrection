4deca76bae334ee187aafbdd235517914cd065d7
willeighteen <59422050+willeighteen@users.noreply.github.com>
2020-01-01 23:29:09 +0000

    Add files via upload

---
+++ data/Cello.arco.sulC/Cello.arco.sulC-header.sco
@@ -0,0 +1,13 @@
+; Cello.arco.sulC-header.sco
+
+; This file should be included in the instrument score files as the first line:
+; #include "data/Cello.arco.sulC/Cello.arco.sulC-header.sco"
+
+#include "header.sco"
+#include "data/Cello.arco.sulC/Cello.arco.sulC.data"
+#include "data/Cello.arco.sulC/Cello.arco.sulC-hmag.data"
+
+; Additional header files may be specified, e.g.
+; #include "data/Cello.arco.sulC/Cello.arco.sulC.terrain"
+
+i1	0	0	143	; i1 defines virtual instr i90 as Cello.arco.sulC
---
+++ data/Cello.arco.sulC/runHetro.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+harmonics=15
+lfpoints=32
+hfpoints=16
+
+# pplf:
+hetro -f65.406 -h$harmonics -m0 -n$lfpoints notes/Cello.arco.pp.sulC.C2.wav ppC2.het
+
+# pphf:
+hetro -f146.83 -h$harmonics -m0 -n$hfpoints notes/Cello.arco.pp.sulC.D5.wav ppD5.het
+
+# fflf:
+hetro -f65.406 -h$harmonics -m0 -n$lfpoints notes/Cello.arco.ff.sulC.C2.wav ffC2.het
+
+# ffhf:
+hetro -f175 -h$harmonics -m0 -n$hfpoints notes/Cello.arco.ff.sulC.B3.wav ffB3.het
+
+unset hfpoints
+unset lfpoints
+unset harmonics
+
